9437547ffee28a420dea28f4269cf749d1ea76e1
Hong Tai Wei <weihongtai@hotmail.com>
2018-07-24 18:05:38 -0700

    cs fiesta

--- cloudscript.js
+++ cloudscript.js
@@ -1,8 +1,8 @@
 var imgur_prefix = "https://i.imgur.com/"
 
 handlers.getMeme = function (args, context) {
-    log.debug("hi");
-    var memes = server.GetTitleInternalData({"Key": "memes"});
-    log.info(memes);
+    var memes = server.GetTitleInternalData({"Key": "memes"})["Data"]["memes"];
+    log.info(len(memes));
+    log.info(memes[0]);
     return { Length: memes.length };
 }
