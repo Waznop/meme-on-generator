e4152612f683c61e7cdc545422d97f6cf0fd715d
Hong Tai Wei <weihongtai@hotmail.com>
2018-07-25 15:22:57 -0700

    pull out function

--- cloudscript.js
+++ cloudscript.js
@@ -1,5 +1,26 @@
 var imgur_prefix = "https://i.imgur.com/"
 
+function updateCoins(delta) {
+    var stats = server.GetPlayerStatistics({
+        PlayFabId: currentPlayerId,
+        StatisticNames: ["coins"]
+    })["Statistics"];
+
+    var numCoins = 0;
+    if (stats.length > 0) {
+        numCoins = stats[0]["Value"];
+    }
+    numCoins += delta;
+
+    server.UpdatePlayerStatistics({
+        PlayFabId: currentPlayerId,
+        Statistics: [{
+            StatisticName: "coins",
+            Value: numCoins
+        }]
+    });
+}
+
 handlers.getMeme = function (args, context) {
     var memes = JSON.parse(server.GetTitleInternalData({"Key": "memes"})["Data"]["memes"]);
     var idx = Math.floor(Math.random() * memes.length);
@@ -10,32 +31,7 @@ handlers.getMeme = function (args, context) {
 
 handlers.skipMeme = function (args, context) {
     var price = args.price;
-
-    var stats = server.GetPlayerStatistics({
-        PlayFabId: currentPlayerId,
-        StatisticNames: ["coins"]
-    })["Statistics"];
-
-    log.info(stats);
-
-    var numCoins = 0;
-    if (stats.length > 0) {
-        numCoins = stats[0]["Value"];
-    }
-    
-    log.info(numCoins);
-
-    numCoins += price;
-
-    log.info(numCoins);
-    
-    server.UpdatePlayerStatistics({
-        PlayFabId: currentPlayerId,
-        Statistics: [{
-            StatisticName: "coins",
-            Value: numCoins
-        }]
-    });
+    updateCoins(price);
 }
 
 handlers.buyMeme = function (args, context) {
